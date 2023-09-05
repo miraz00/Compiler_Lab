44bc9c56b232eec7abd3b30ca4c40677694e96cf
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2023-09-05 14:55:06 +0530

    Update lex_analyzer.c

--- Lexical_Analyzer/lex_analyzer.c
+++ Lexical_Analyzer/lex_analyzer.c
@@ -4,23 +4,42 @@
 
 int main()
 {
-    FILE *filein = fopen("filein.txt", "r"); // open file for the input
-    FILE *fileout = fopen("fileout.txt", "w"); // open file for output
-    int lineno = 1, tokno = 0, i,j, flag = 0; // lineno to count the number of lines have been covered and tokno is the count of tokens that is read
-    char ch, str[100], keyword[50][50] = {"int", "main", "if", "else", "while", "for", "return", "do", "switch", "FILE", "printf", "scanf"}; // Common keywords in C language
-    fprintf(fileout,"Line \t\t Token no. \t Token \t\t Lexeme\n"); //printing the header of the table
-
+    FILE *filein = fopen("filein.txt", "r"); 
+    FILE *fileout = fopen("fileout.txt", "w"); 
+    int lineno = 1, tokno = 0, i,j, flag = 0; 
+    char ch, str[100], keyword[50][50] = {"int", "main", "if", "else", "while", "for", "return", "do", "switch", "FILE", "printf", "scanf"}; 
+    fprintf(fileout,"Line \t\t Token no. \t Token \t\t Lexeme\n"); 
+    
     while (!feof(filein))
     {
-        i = 0; flag = 0;
+    
+    if(ch == '/')
+    {
+    	while((ch = fgetc(filein))!= '\n')
+    	{
+    		if(ch == '\n')
+    			{break;}
+    	}
+    }
+    else if(ch == '*')
+    {
+    	while((ch = fgetc(filein))!= '*')
+    	if (ch == EOF)
+    	{break;}
+    	ch = fgetc(filein);
+    	if(ch == '/')
+    	{continue;}
+    }
+    
+       i = 0; flag = 0;
         ch = fgetc(filein);
-        if (ch=='+' || ch=='-' || ch=='*' || ch=='/' || ch=='%')
+        if (ch=='+' || ch=='-' || ch=='*' || ch=='/' || ch=='%' || ch == '=')
         {
                         fprintf(fileout,"%d\t\t %d\t\t Operator\t %c\n",lineno,tokno,ch);
                         tokno++;
         }
         
-        else if ( ch==';' || ch=='{' || ch=='}' || ch=='?' || ch=='|' || ch=='[' || ch==']'|| ch==':')
+        else if ( ch==';' || ch=='{' || ch=='}' || ch=='?' || ch=='|' || ch=='[' || ch==']'|| ch==':' || ch == '\"'|| ch == '(' || ch == ')' || ch == '>' || ch == '<')
         {
             fprintf(fileout,"%d\t\t %d\t\t Special Symbol\t %c\n",lineno,tokno,ch);
             tokno++;
@@ -31,19 +50,19 @@ int main()
                         fprintf(fileout,"%d\t\t %d\t\t Digit\t\t %c\n",lineno,tokno,ch);
                         tokno++;
         }
-        
+               
         else if (isalpha(ch))
         {
-            str[i] = ch;
-            i++;
+            str[i++] = ch;
             ch = fgetc(filein);
 
             while (isalnum(ch) && ch!=' ') 
             {
-                str[i] = ch;
-                i++;
+                str[i++] = ch;
                 ch = fgetc(filein);
+                
             }
+         
             str[i] = '\0';
 
             for ( j = 0; j<50; j++)
@@ -55,13 +74,19 @@ int main()
                 }
             }
             
-            if (flag == 1)
+     if (flag == 1)
             {
                 fprintf(fileout,"%d\t\t %d\t\t Keyword\t %s\n",lineno,tokno,str);
+                tokno++;
             }
-            else {fprintf(fileout,"%d\t\t %d\t\t Identifier\t %s\n",lineno,tokno,str);}
-
-            tokno++;
+            else {fprintf(fileout,"%d\t\t %d\t\t Identifier\t %s\n",lineno,tokno,str);
+            tokno++;}
+	   if ( ch=='{' || ch=='}' || ch=='?' || ch=='|' || ch=='[' || ch==']'|| ch==':' || ch == '\"'|| ch == '(' || ch == ')')
+        	{
+           	 fprintf(fileout,"%d\t\t %d\t\t Special Symbol\t %c\n",lineno,tokno,ch);
+            	tokno++;
+       		 }
+            
             
         }
         else if(ch == '\n')

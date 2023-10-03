16327c4e4bc784f358c2e1308ea11810d4553556
ashwin417 <ashwinr7603@gmail.com>
2023-10-03 13:44:09 +0530

    hello

--- Operator Precedence/spam/op_pre_par.c
+++ Operator Precedence/spam/op_pre_par.c
@@ -1,6 +1,6 @@
-//This is not correct, check the other program oppre2.c
-#include<stdio.h>
-#include<string.h>
+// This is not correct, check the other program oppre2.c
+#include <stdio.h>
+#include <string.h>
 
 #define MAX 100
 
@@ -11,15 +11,18 @@ char ter[MAX];
 
 int top = -1; // Initialize top to -1
 
-int main() {
+int main()
+{
   int i, j, k, n, col, row;
 
   // Initialize arrays
-  for (i = 0; i < MAX; i++) {
+  for (i = 0; i < MAX; i++)
+  {
     stack[i] = '\0';
     ip[i] = '\0';
     ter[i] = '\0';
-    for (j = 0; j < MAX; j++) {
+    for (j = 0; j < MAX; j++)
+    {
       opt[i][j][0] = '\0';
       opt[i][j][1] = '\0';
     }
@@ -32,21 +35,26 @@ int main() {
   scanf("%s", ter);
 
   printf("\nEnter the table values :\n");
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < n; j++) {
+  for (i = 0; i < n; i++)
+  {
+    for (j = 0; j < n; j++)
+    {
       printf("Enter the value for %c %c:", ter[i], ter[j]);
       scanf("%s", opt[i][j]);
     }
   }
 
   printf("\n**** OPERATOR PRECEDENCE TABLE ****\n");
-  for (i = 0; i < n; i++) {
+  for (i = 0; i < n; i++)
+  {
     printf("\t%c", ter[i]);
   }
   printf("\n");
-  for (i = 0; i < n; i++) {
+  for (i = 0; i < n; i++)
+  {
     printf("\n%c", ter[i]);
-    for (j = 0; j < n; j++) {
+    for (j = 0; j < n; j++)
+    {
       printf("\t%s", opt[i][j]);
     }
   }
@@ -59,40 +67,54 @@ int main() {
   printf("\nSTACK\t\t\tINPUT STRING\t\t\tACTION\n");
   printf("\n%s\t\t\t%s\t\t\t", stack, ip);
 
-  while (i < strlen(ip)) { // Change to "<" from "<=" to prevent buffer overflow
+  while (i < strlen(ip))
+  { // Change to "<" from "<=" to prevent buffer overflow
     col = -1;
     row = -1;
 
-    for (k = 0; k < n; k++) {
-      if (stack[top] == ter[k]) {
+    for (k = 0; k < n; k++)
+    {
+      if (stack[top] == ter[k])
+      {
         col = k;
       }
-      if (ip[i] == ter[k]) {
+      if (ip[i] == ter[k])
+      {
         row = k;
       }
     }
 
-    if (col == -1 || row == -1) {
+    if (col == -1 || row == -1)
+    {
       printf("\nString is not accepted");
       break;
     }
 
-    if ((stack[top] == '$') && (ip[i] == '$')) {
+    if ((stack[top] == '$') && (ip[i] == '$'))
+    {
       printf("String is accepted\n");
       break;
-    } else if ((opt[col][row][0] == '<') || (opt[col][row][0] == '=')) {
+    }
+    else if ((opt[col][row][0] == '<') || (opt[col][row][0] == '='))
+    {
       stack[++top] = opt[col][row][0];
       stack[++top] = ip[i];
       printf("Shift %c", ip[i]);
       i++;
-    } else {
-      if (opt[col][row][0] == '>') {
-        while (stack[top] != '<') {
+    }
+    else
+    {
+      if (opt[col][row][0] == '>')
+      {
+        while (stack[top] != '<')
+        {
           top--;
         }
         top--;
         printf("Reduce");
-      } else {
+      }
+      else
+      {
         printf("\nString is not accepted");
         break;
       }
